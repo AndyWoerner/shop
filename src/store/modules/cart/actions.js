e1c15bc4ff96b82515080edacae97c649206739d
Andy Woerner <andy.woerner@icloud.com>
2020-07-09 16:57:36 +0200

    refactor: outsource actions in cart store

---
+++ src/store/modules/cart/actions.js
@@ -0,0 +1,29 @@
+import axios from "axios"
+import * as types from "./mutation-types"
+
+export const actions = {
+    getCartItems({commit}) {
+        axios.get("/api/cart")
+            .then((response) => {
+                commit(types.UPDATE_CART_ITEMS, response.data)
+            })
+    },
+    addCartItem({ commit }, payload){
+        axios.post("/api/cart", payload)
+        .then((response) => {
+            commit(types.UPDATE_CART_ITEMS, response.data)
+        }) //wenn wir einen Datensatz hinzufügen in den Warenkorb, bekommen wir den kompletten Inhalt des aktuellen Warenkorb zurückgeliefert
+    },
+    removeCartItem({ commit }, payload){
+        axios.post("/api/cart/delete", payload)
+        .then((response) => {
+            commit(types.UPDATE_CART_ITEMS, response.data)
+        }) //Beim Löschen des Items wird ein spezieller Endpoint genutzt. Auf dem Server passiert das Löschen mit Hilfe der ID. Zurück kommt wieder der aktuelle Warenkorb
+    },
+    removeAllCartItems ({commit}) {
+        axios.post("/api/cart/delete/all")
+            .then((response) => {
+                commit(types.UPDATE_CART_ITEMS, response.data)
+            })
+    }
+}
--- src/store/modules/cart/index.js
+++ src/store/modules/cart/index.js
@@ -1,5 +1,7 @@
-import axios from "axios"
 import * as types from "./mutation-types"
+//die ausgelagerten actions werden hier importiert
+//funktioniert genauso mit getters, mutations, state usw... wird übersichtlicher
+import { actions } from "./actions"
 
 const state = {
     cartItems: []
@@ -8,33 +10,6 @@ const state = {
 const mutations = {
     [types.UPDATE_CART_ITEMS](state, payload) {
         state.cartItems = payload
-    }
-}
-
-const actions = {
-    getCartItems({commit}) {
-        axios.get("/api/cart")
-            .then((response) => {
-                commit(types.UPDATE_CART_ITEMS, response.data)
-            })
-    },
-    addCartItem({ commit }, payload){
-        axios.post("/api/cart", payload)
-        .then((response) => {
-            commit(types.UPDATE_CART_ITEMS, response.data)
-        }) //wenn wir einen Datensatz hinzufügen in den Warenkorb, bekommen wir den kompletten Inhalt des aktuellen Warenkorb zurückgeliefert
-    },
-    removeCartItem({ commit }, payload){
-        axios.post("/api/cart/delete", payload)
-        .then((response) => {
-            commit(types.UPDATE_CART_ITEMS, response.data)
-        }) //Beim Löschen des Items wird ein spezieller Endpoint genutzt. Auf dem Server passiert das Löschen mit Hilfe der ID. Zurück kommt wieder der aktuelle Warenkorb
-    },
-    removeAllCartItems ({commit}) {
-        axios.post("/api/cart/delete/all")
-            .then((response) => {
-                commit(types.UPDATE_CART_ITEMS, response.data)
-            })
     }
 }
 
