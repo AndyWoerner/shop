c1f70e016b93ff716cf655b6b104a34f7e1eb218
Andy Woerner <andy.woerner@icloud.com>
2020-07-09 15:24:39 +0200

    refactor: add stores for cart and product modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+// Module importieren
+import cart from "./modules/cart"
+import product from "./modules/product"
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  // state: {
+  // },
+  // mutations: {
+  // },
+  // actions: {
+  // },
   modules: {
+    cart,
+    product
   }
 })
---
+++ src/store/modules/product/index.js
@@ -0,0 +1,37 @@
+import axios from "axios"
+
+const state = {
+    productItems: []
+}
+
+const mutations = {
+    //wenn unsere Anwedung startet werden wir daten vom server holen -> productItems werden dann geändert
+    UPDATE_PRODUCT_ITEMS (state, payload) {
+        // payload ist das Array das wir vom Server bekommen
+        state.productItems = payload
+    }
+}
+
+const actions = {
+    getProductItems({ commit }) {
+        //nur Anfrage, daher kein payload
+        axios.get("/api/products")
+            .then((response) => {
+                console.log(response);
+                commit("UPDATE_PRODUCT_ITEMS", response.data)
+            })
+    }
+}
+ 
+const getters = {
+    productItems: state => state.productItems
+}
+
+const productModule = {
+    state,
+    mutations,
+    actions,
+    getters
+}
+
+export default productModule
