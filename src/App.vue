60fe39e52165fc63b88c64c423f3af6e28401a55
Andy Woerner <andy.woerner@icloud.com>
2020-07-12 14:58:42 +0200

    feat: navbar added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-4 p-5">
+      
+      <div class="col-12">
+        <Navbar />
+      </div>
+      
+      <div class="col-4 p-4">
         <CartList />
       </div>
 
@@ -13,11 +18,13 @@
 </template>
 
 <script>
+import Navbar from "./components/Navbar";
 import CartList from "./components/cart/CartList";
 import ProductList from "./components/product/ProductList";
 export default {
   name: "App",
   components: {
+    Navbar,
     CartList,
     ProductList
   }
